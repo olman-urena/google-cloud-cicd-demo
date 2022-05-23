afe7b2446dec8a74d2e51d2afcedbd1b58768465
Olman Urena Herrera <olman.urena@ibm.com>
2022-05-23 17:18:30 -0600

    Add demo project

---
+++ google-cloud-cicd-demo/src/main/java/com/demo/googlecloudcicddemo/GoogleCloudCicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.demo.googlecloudcicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleCloudCicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleCloudCicdDemoApplication.class, args);
+	}
+
+}
---
+++ google-cloud-cicd-demo/src/test/java/com/demo/googlecloudcicddemo/GoogleCloudCicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.googlecloudcicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleCloudCicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
